5bf16714bccb80b1b25fc95dabb9272e5add70fd
Chance093 <chance.dev093@gmail.com>
2026-06-25 16:09:11 -0700

    create boilerplate for discord client

--- internal/lib/discord.go
+++ internal/lib/discord.go
@@ -2,6 +2,19 @@ package lib
 
 import "fmt"
 
-func Discord() {
-	fmt.Println("discord")
+type DiscordClient struct {
+	TOKEN string
+}
+
+func NewDiscordClient(token string) DiscordClient {
+	return DiscordClient{TOKEN: token}
+}
+
+func (dc *DiscordClient) PostMessage(message string) {
+	dc.formatMessage()
+	fmt.Println("printing message to discord")
+}
+
+func (dc *DiscordClient) formatMessage() {
+	fmt.Println("formatting message")
 }
