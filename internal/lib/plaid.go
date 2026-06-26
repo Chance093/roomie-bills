ffd96e5920044d78dc18385053000ce24a41c5bb
Chance093 <chance.dev093@gmail.com>
2026-06-25 17:13:27 -0700

    add more to plaid lib

--- internal/lib/plaid.go
+++ internal/lib/plaid.go
@@ -11,6 +11,20 @@ func NewPlaidClient(id, secret string) PlaidClient {
 	return PlaidClient{CLIENT_ID: id, SECRET: secret}
 }
 
-func (pc *PlaidClient) GetPastTransactions() {
+func (pc *PlaidClient) GetNewTransactions() {
+	pc.getAllTransactions()
 	fmt.Println("getting past transactions")
 }
+
+func (pc *PlaidClient) getAllTransactions() {
+	// request from plaid api
+	fmt.Println("getting all transactions")
+	// filter for new transactions
+	filterTransactions()
+}
+
+func filterTransactions() {
+	// find all new transactions
+}
+
+
