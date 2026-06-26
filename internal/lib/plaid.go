3f7b2a7ad615b3e3a9b9c0bf82a9e6caf59f8292
Chance093 <chance.dev093@gmail.com>
2026-06-26 13:48:59 -0700

    start initial logic for creating link tokens with plaid

--- internal/lib/plaid.go
+++ internal/lib/plaid.go
@@ -1,7 +1,8 @@
 package lib
 
 import (
-	"fmt"
+	"context"
+
 	"github.com/plaid/plaid-go/v43/plaid"
 )
 
@@ -11,23 +12,57 @@ type PlaidClient struct {
 
 func NewPlaidClient(id, secret string) PlaidClient {
 	configuration := plaid.NewConfiguration()
-	configuration.AddDefaultHeader("PLAID-CLIENT-ID", id) 
-	configuration.AddDefaultHeader("PLAID-SECRET", secret) 
+	configuration.AddDefaultHeader("PLAID-CLIENT-ID", id)
+	configuration.AddDefaultHeader("PLAID-SECRET", secret)
 	configuration.UseEnvironment(plaid.Sandbox)
 	client := plaid.NewAPIClient(configuration)
+
 	return PlaidClient{client}
 }
 
 func (pc *PlaidClient) GetNewTransactions() {
-	pc.getAllRecentTransactions()
-	fmt.Println("getting past transactions")
 }
 
-func (pc *PlaidClient) getAllRecentTransactions() {
-	// request from plaid api
-	fmt.Println("getting all transactions")
-}
+func (pc *PlaidClient) GetAccessToken(userName string) {
+	user := plaid.LinkTokenCreateRequestUser{
+		ClientUserId: userName,
+	}
+	transactions := plaid.LinkTokenTransactions{}
+	depository := plaid.DepositoryFilter{
+		AccountSubtypes: []plaid.DepositoryAccountSubtype{
+			plaid.DEPOSITORYACCOUNTSUBTYPE_CHECKING,
+		},
+	}
+	credit := plaid.CreditFilter{
+		AccountSubtypes: []plaid.CreditAccountSubtype{plaid.CREDITACCOUNTSUBTYPE_CREDIT_CARD},
+	}
+	accountFilters := plaid.LinkTokenAccountFilters{
+		Depository: &depository,
+		Credit:     &credit,
+	}
+	request := plaid.NewLinkTokenCreateRequest(
+		"Roomie Bills",
+		"en",
+		[]plaid.CountryCode{plaid.COUNTRYCODE_US},
+	)
+	request.SetProducts([]plaid.Products{plaid.PRODUCTS_TRANSACTIONS})
+	request.SetTransactions(transactions)
+	request.SetWebhook("https://sample-web-hook.com")
+	request.SetRedirectUri("https://domainname.com/oauth-page.html")
+	request.SetAccountFilters(accountFilters)
 
-func filterTransactions() {
-	// find all new transactions
+	ctx := context.Background()
+	linkTokenCreateResp, _, err := pc.client.PlaidApi.LinkTokenCreate(ctx).LinkTokenCreateRequest(*request).Execute()
+	if err != nil {
+		panic(err)
+	}
+	linkToken := linkTokenCreateResp.GetLinkToken()
+
+	exchangePublicTokenReq := plaid.NewItemPublicTokenExchangeRequest(linkToken)
+	exchangePublicTokenResp, _, err := pc.client.PlaidApi.ItemPublicTokenExchange(ctx).ItemPublicTokenExchangeRequest(
+		*exchangePublicTokenReq,
+	).Execute()
+	accessToken := exchangePublicTokenResp.GetAccessToken()
+
+	// TODO: Save access token to db
 }
--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/Chance093/roomie-bills/internal/lib"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  plaidClientId := os.Getenv("PLAID_CLIENT_ID")
+  plaidSecretKey := os.Getenv("PLAID_SANDBOX_SECRET")
+
+	pc := lib.NewPlaidClient(plaidClientId, plaidSecretKey)
+
+	pc.GetAccessToken("Chance")
 }
 
