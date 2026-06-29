32a874dc6b99440457aa7f806f2c7d1ff7325095
Chance093 <chance.dev093@gmail.com>
2026-06-28 17:53:01 -0700

    inject db into server struct

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,21 +1,27 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Chance093/roomie-bills/internal/db"
+)
 
 type Server struct {
 	Router *http.ServeMux
 	Addr   string
 	Env    map[string]string
+	DB     *db.DB
 }
 
 // NewServer intializes a server, sets up routes, and allows database access
 // to all handlers associated with that server.
-func NewServer(port string, env map[string]string) *Server {
+func NewServer(port string, db *db.DB, env map[string]string) *Server {
 	// init server
 	s := &Server{
 		Router: http.NewServeMux(),
 		Addr:   ":" + port,
 		Env:    env,
+		DB: db,
 	}
 
 	s.Router.HandleFunc("GET /webhook/plaid", exampleHandler)
