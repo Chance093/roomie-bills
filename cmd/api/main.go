70623b4edb74cf0d29f41c63b28638e729970fb4
Chance093 <chance.dev093@gmail.com>
2026-07-03 00:17:15 -0700

    set up server in api cmd

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Chance093/roomie-bills/internal/api"
+	"github.com/Chance093/roomie-bills/internal/db"
+)
+
+const port = "8080"
+
 func main() {
 	// init and run server
-}
+	db := db.NewDB()
+	defer db.Close()
 
+	s := api.NewServer(port, db)
+	fmt.Printf("Serving on port :%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, s.Router))
+}
--- internal/api/server.go
+++ internal/api/server.go
@@ -9,18 +9,16 @@ import (
 type Server struct {
 	Router *http.ServeMux
 	Addr   string
-	Env    map[string]string
 	DB     *db.DB
 }
 
 // NewServer intializes a server, sets up routes, and allows database access
 // to all handlers associated with that server.
-func NewServer(port string, db *db.DB, env map[string]string) *Server {
+func NewServer(port string, db *db.DB) *Server {
 	// init server
 	s := &Server{
 		Router: http.NewServeMux(),
 		Addr:   ":" + port,
-		Env:    env,
 		DB: db,
 	}
 
