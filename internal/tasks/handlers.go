4b23fd686f5a511f59de60395ee03da6fae7d15e
Chance093 <chance.dev093@gmail.com>
2026-09-04 10:25:50 -0700

    add task handlers for cron jobs

--- internal/tasks/handlers.go
+++ internal/tasks/handlers.go
@@ -4,20 +4,24 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 
 	"github.com/Chance093/roomie-bills/internal/db"
+	"github.com/Chance093/roomie-bills/internal/lib"
 	"github.com/Chance093/roomie-bills/internal/lib/bgjobs"
 	"github.com/Chance093/roomie-bills/internal/lib/plaid"
+	"github.com/Chance093/roomie-bills/internal/types"
 )
 
 type Handler struct {
 	pc plaid.Client
 	jc bgjobs.Client
+	dc lib.DiscordClient
 	db *db.DB
 }
 
-func NewHandler(pc plaid.Client, jc bgjobs.Client, db *db.DB) Handler {
-	return Handler{pc, jc, db}
+func NewHandler(pc plaid.Client, jc bgjobs.Client, dc lib.DiscordClient, db *db.DB) Handler {
+	return Handler{pc, jc, dc, db}
 }
 
 func (h Handler) GetAccessToken(ctx context.Context, t bgjobs.Task) error {
@@ -137,4 +141,136 @@ func (h Handler) AddAccounts(ctx context.Context, t bgjobs.Task) error {
 	}
 
 	return nil
+}
+
+func (h Handler) GetAccessTokens(ctx context.Context, t bgjobs.Task) error {
+	// get access tokens from bank table in db
+	accessTokens, err := h.db.GetBankAccessTokens()
+	if err != nil {
+		return fmt.Errorf("Error while getting bank access tokens: %w", err)
+	}
+
+	// create new task and enqueue
+	newTask, err := NewGetBillsTask(accessTokens)
+	if err != nil {
+		return err
+	}
+
+	if _, err := h.jc.Enqueue(newTask); err != nil {
+		return fmt.Errorf("Could not enqueue new task: %w", err)
+	}
+
+	return nil
+}
+
+func (h Handler) GetBills(ctx context.Context, t bgjobs.Task) error {
+	// get payload
+	var payload GetBillsPayload
+	if err := json.Unmarshal(t.Payload, &payload); err != nil {
+		return fmt.Errorf("Could not unmarshal json into payload: %w", err)
+	}
+
+	// get bills from plaid using access tokens
+	plaidBills, err := h.pc.GetBills(ctx, payload.AccessTokens)
+	if err != nil {
+		return fmt.Errorf("Error while getting bills from plaid: %w", err)
+	}
+
+	// create new task and enqueue
+	newTask, err := NewGetNewBillsTask(plaidBills)
+	if err != nil {
+		return err
+	}
+
+	if _, err := h.jc.Enqueue(newTask); err != nil {
+		return fmt.Errorf("Could not enqueue new task: %w", err)
+	}
+
+	return nil
+}
+
+func (h Handler) GetNewBills(ctx context.Context, t bgjobs.Task) error {
+	// get payload
+	var payload GetNewBillsPayload
+	if err := json.Unmarshal(t.Payload, &payload); err != nil {
+		return fmt.Errorf("Could not unmarshal json into payload: %w", err)
+	}
+
+	// find new bills
+	newPlaidBills, err := h.db.FindNewPlaidBills(payload.PlaidBills)
+	if err != nil {
+		return fmt.Errorf("Error while getting non existing bills: %w", err)
+	}
+
+	// create new task and enqueue depending on if there are bills or not
+	var newTask *bgjobs.Task
+	if len(newPlaidBills) == 0 {
+		newTask, err = NewSendNoBillsTask()
+	} else {
+		newTask, err = NewAddBillsPaymentsTask(newPlaidBills)
+	}
+	if err != nil {
+		return err
+	}
+
+	if _, err := h.jc.Enqueue(newTask); err != nil {
+		return fmt.Errorf("Could not enqueue new task: %w", err)
+	}
+
+	return nil
+}
+
+func (h Handler) AddBillsAndPayments(ctx context.Context, t bgjobs.Task) error {
+	// get payload
+	var payload AddBillsPaymentsPayload
+	if err := json.Unmarshal(t.Payload, &payload); err != nil {
+		return fmt.Errorf("Could not unmarshal json into payload: %w", err)
+	}
+
+	// insert payments for bill owners
+	bills, err := h.db.AddBillsAndPayments(payload.PlaidBills)
+	if err != nil {
+		return fmt.Errorf("Error while adding bills and payments; %w", err)
+	}
+
+	// create new task and enqueue
+	newTask, err := NewSendBillsTask(bills)
+	if err != nil {
+		return err
+	}
+
+	if _, err := h.jc.Enqueue(newTask); err != nil {
+		return fmt.Errorf("Could not enqueue new task: %w", err)
+	}
+
+	return nil
+}
+
+func (h Handler) SendBills(ctx context.Context, t bgjobs.Task) error {
+	// get payload
+	var payload SendBillsPayload
+	if err := json.Unmarshal(t.Payload, &payload); err != nil {
+		return fmt.Errorf("Could not unmarshal json into payload: %w", err)
+	}
+
+	// split bills 4 ways
+	splitBills := splitBills(payload.Bills)
+
+	// Send discord message notifying chat of new bills
+	if err := h.dc.SendBills(splitBills); err != nil {
+		return fmt.Errorf("Error while sending bills to discord: %w", err)
+	}
+
+	return nil
+}
+
+// TODO: use decimal package
+func splitBills(bills []db.Bill) []types.SplitBill {
+	splitBills := make([]types.SplitBill, len(bills))
+	for i, bill := range bills {
+		split := math.Round(bill.Total/4*100) / 100
+		splitBills[i] = types.SplitBill{Bill: bill, Split: split}
+	}
+
+	return splitBills
 }
