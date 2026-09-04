65d72adba1f98dd60a34a1b36a609f81b4b1188e
Chance093 <chance.dev093@gmail.com>
2026-09-04 10:09:18 -0700

    add new task initializers

--- internal/tasks/tasks.go
+++ internal/tasks/tasks.go
@@ -4,42 +4,68 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/Chance093/roomie-bills/internal/db"
 	"github.com/Chance093/roomie-bills/internal/lib/bgjobs"
 	"github.com/Chance093/roomie-bills/internal/lib/plaid"
 )
 
+// TODO: find better naming convention for these
 const (
-	TypeGetAccessToken = "get:accessToken"
-	TypeGetBank        = "get:bank"
-	TypeUpdateBank     = "update:bank"
-	TypeGetAccounts    = "get:accounts"
-	TypeAddAccounts    = "add:accounts"
+	TypeGetAccessToken   = "get:accessToken"
+	TypeGetBank          = "get:bank"
+	TypeUpdateBank       = "update:bank"
+	TypeGetAccounts      = "get:accounts"
+	TypeAddAccounts      = "add:accounts"
+	TypeGetAccessTokens  = "get:accessTokens"
+	TypeGetBills         = "get:bills"
+	TypeGetNewBills      = "get:new:bills"
+	TypeAddBillsPayments = "add:bills:payments"
+	TypeSendBills        = "send:bills"
+	TypeSendNoBills      = "send:nothing"
 )
 
-type GetAccessTokenPayload struct {
-	PublicToken string `json:"publicToken"`
-	LinkToken   string `json:"linkToken"`
-}
+type (
+	GetAccessTokenPayload struct {
+		PublicToken string `json:"publicToken"`
+		LinkToken   string `json:"linkToken"`
+	}
 
-type GetBankPayload struct {
-	AccessToken plaid.AccessToken `json:"accessToken"`
-	LinkToken   string            `json:"linkToken"`
-}
+	GetBankPayload struct {
+		AccessToken plaid.AccessToken `json:"accessToken"`
+		LinkToken   string            `json:"linkToken"`
+	}
 
-type UpdateBankPayload struct {
-	GetBankPayload
-	Bank string `json:"bank"`
-}
+	UpdateBankPayload struct {
+		GetBankPayload
+		Bank string `json:"bank"`
+	}
 
-type GetAccountsPayload struct {
-	BankId      int
-	AccessToken string
-}
+	GetAccountsPayload struct {
+		BankId      int    `json:"bankId"`
+		AccessToken string `json:"accessToken"`
+	}
 
-type AddAccountsPayload struct {
-	BankId   int
-	Accounts []plaid.Account
-}
+	AddAccountsPayload struct {
+		BankId   int             `json:"bankId"`
+		Accounts []plaid.Account `json:"accounts"`
+	}
+
+	GetBillsPayload struct {
+		AccessTokens []string `json:"accessTokens"`
+	}
+
+	GetNewBillsPayload struct {
+		PlaidBills []plaid.Bill `json:"plaidBills"`
+	}
+
+	AddBillsPaymentsPayload struct {
+		GetNewBillsPayload
+	}
+
+	SendBillsPayload struct {
+		Bills []db.Bill `json:"bills"`
+	}
+)
 
 func newTask(v any, taskType string) (*bgjobs.Task, error) {
 	p, err := json.Marshal(v)
@@ -96,4 +122,44 @@ func NewAddAccountsTask(accounts []plaid.Account, bankId int) (*bgjobs.Task, err
 	}
 
 	return newTask(v, TypeAddAccounts)
+}
+
+func NewGetAccessTokensTask() (*bgjobs.Task, error) {
+	return newTask("", TypeGetAccessTokens)
+}
+
+func NewGetBillsTask(accessTokens []string) (*bgjobs.Task, error) {
+	v := GetBillsPayload{
+		AccessTokens: accessTokens,
+	}
+
+	return newTask(v, TypeGetBills)
+}
+
+func NewGetNewBillsTask(plaidBills []plaid.Bill) (*bgjobs.Task, error) {
+	v := GetNewBillsPayload{
+		PlaidBills: plaidBills,
+	}
+
+	return newTask(v, TypeGetNewBills)
+}
+
+func NewAddBillsPaymentsTask(plaidBills []plaid.Bill) (*bgjobs.Task, error) {
+	v := AddBillsPaymentsPayload{
+		GetNewBillsPayload{PlaidBills: plaidBills},
+	}
+
+  return newTask(v, TypeAddBillsPayments)
+}
+
+func NewSendBillsTask(bills []db.Bill) (*bgjobs.Task, error) {
+  v := SendBillsPayload{
+    Bills: bills,
+  }
+
+  return newTask(v, TypeSendBills)
+}
+
+func NewSendNoBillsTask() (*bgjobs.Task, error) {
+  return newTask("", TypeSendNoBills)
 }
