d78963975e20f55628bd3dedc2a31ab4c4630f51
Chance093 <chance.dev093@gmail.com>
2026-09-21 12:28:36 -0700

    add Enqueue, Claim, and Complete methods to task queue

--- internal/lib/taskqueue/taskqueue.go
+++ internal/lib/taskqueue/taskqueue.go
@@ -1,16 +1,18 @@
 package taskqueue
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/redis/go-redis/v9"
 )
 
-type TaskQueue struct {
+type taskQueue struct {
 	redis *redis.Client
 
-	queueName   string
-	maxAttempts int
+	Options
 
 	pendingKey    string
 	processingKey string
@@ -20,16 +22,24 @@ type TaskQueue struct {
 }
 
 type Options struct {
-	queueName   string
-	maxAttempts int
+	queueName        string
+	maxAttempts      int
+	completedTTL     int
+	completedHistory int
 }
 
-func New(opts Options) *TaskQueue {
+func newTaskQueue(opts Options) *taskQueue {
 	if opts.queueName == "" {
 		opts.queueName = "tasks"
 	}
 	if opts.maxAttempts <= 0 {
 		opts.maxAttempts = 3
+	}
+	if opts.completedTTL <= 0 {
+		opts.completedTTL = 300
+	}
+	if opts.completedHistory <= 0 {
+		opts.completedHistory = 50
 	}
 
 	rdb := redis.NewClient(&redis.Options{
@@ -37,11 +47,15 @@ func New(opts Options) *TaskQueue {
 		Protocol: 2,
 	})
 
-	return &TaskQueue{
+	return &taskQueue{
 		redis: rdb,
 
-		queueName:   opts.queueName,
-		maxAttempts: opts.maxAttempts,
+		Options: Options{
+			queueName:        opts.queueName,
+			maxAttempts:      opts.maxAttempts,
+			completedTTL:     opts.completedTTL,
+			completedHistory: opts.completedHistory,
+		},
 
 		pendingKey:    fmt.Sprintf("queue:%s:pending", opts.queueName),
 		processingKey: fmt.Sprintf("queue:%s:processing", opts.queueName),
@@ -49,4 +63,104 @@ func New(opts Options) *TaskQueue {
 		failedKey:     fmt.Sprintf("queue:%s:failed", opts.queueName),
 		taskPrefix:    fmt.Sprintf("queue:%s:task:", opts.queueName),
 	}
+}
+
+func (q *taskQueue) Enqueue(ctx context.Context, t Task) (string, error) {
+	// update task properties
+	taskId := t.Id
+	t.EnqueuedAtMs = nowMs()
+	t.Status = "pending"
+
+	// set task hash and push to pending task queue in redis
+	pipe := q.redis.Pipeline()
+	pipe.HSet(ctx, q.taskKey(taskId), t)
+	pipe.LPush(ctx, q.pendingKey, taskId)
+	if _, err := pipe.Exec(ctx); err != nil {
+		return "", fmt.Errorf("Error while enqueueing task in redis: %w", err)
+	}
+
+	return taskId, nil
+}
+
+type ClaimedTask struct {
+	Id         string
+	Payload    string
+	Attempts   int
+	ClaimToken string
+}
+
+func (q *taskQueue) Claim(ctx context.Context) (*ClaimedTask, error) {
+	// move task id from pending task queue to processing task queue
+	taskId, err := q.redis.BLMove(ctx, q.pendingKey, q.processingKey, "RIGHT", "LEFT", time.Duration(0)).Result()
+	if err == redis.Nil {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("Error while moving taskId from pending to processing queue: %w", err)
+	}
+
+	// get task hash from redis and update task properties
+	var t Task
+	taskKey := q.taskKey(taskId)
+	if err := q.redis.HGetAll(ctx, taskKey).Scan(&t); err != nil {
+		return nil, fmt.Errorf("Error while getting task (%s) hash from redis: %w", taskId, err)
+	}
+
+	t.Status = "processing"
+	t.ClaimedAtMs = nowMs()
+	t.ClaimToken = uuid.New().String()
+	t.Attempts++
+
+	// update task hash in redis and return claimed task
+	if _, err := q.redis.HSet(ctx, taskKey, t).Result(); err != nil {
+		return nil, fmt.Errorf("Error while updating task (%s) hash in redis: %w", taskId, err)
+	}
+
+	return &ClaimedTask{taskId, t.Payload, t.Attempts, t.ClaimToken}, nil
+}
+
+func (q *taskQueue) Complete(ctx context.Context, task *ClaimedTask) (bool, error) {
+	// compare claim token in claimed task with claim token in redis
+	taskId := task.Id
+	taskKey := q.taskKey(taskId)
+	currentToken, err := q.redis.HGet(ctx, taskKey, "claim_token").Result()
+	if err != nil {
+		return false, fmt.Errorf("Error while getting claim token from task (%s) hash: %w", taskId, err)
+	}
+
+	if currentToken != task.ClaimToken {
+		return false, nil // TODO: error handling (don't know if it should be error)
+	}
+
+	// get task hash from redis and update task properties
+	var t Task
+	if err := q.redis.HGetAll(ctx, taskKey).Scan(&t); err != nil {
+		return false, fmt.Errorf("Error while getting task (%s) hash from redis: %w", taskId, err)
+	}
+
+	t.Status = "completed"
+	t.CompletedAtMs = nowMs()
+
+	// remove task id from processing task queue, update task hash, set expiration 
+	// for task hash, push task id to completed task queue, and trim the completed 
+	// task queue, and execute these commands all at once
+	pipe := q.redis.Pipeline()
+	pipe.LRem(ctx, q.processingKey, 1, taskId)
+	pipe.HSet(ctx, taskKey, t)
+	pipe.Expire(ctx, taskKey, time.Duration(q.completedTTL))
+	pipe.LPush(ctx, q.completedKey, taskId)
+	pipe.LTrim(ctx, q.completedKey, 0, int64(q.completedHistory)-1)
+	if _, err := pipe.Exec(ctx); err != nil {
+		return false, fmt.Errorf("Error while performing pipeline redis calls for completed task: %w", err)
+	}
+
+	return true, nil
+}
+
+func nowMs() int64 {
+	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+func (q taskQueue) taskKey(taskId string) string {
+	return q.taskPrefix + taskId
 }
---
+++ internal/lib/taskqueue/tasks.go
@@ -0,0 +1,28 @@
+package taskqueue
+
+import (
+	"github.com/google/uuid"
+)
+
+type Task struct {
+	Id            string `redis:"id"`
+	Name          string `redis:"name"`
+	Payload       string `redis:"payload"`
+	Status        string `redis:"status"`
+	Attempts      int    `redis:"attempts"`
+	EnqueuedAtMs  int64  `redis:"enqueued_at_ms"`
+	ClaimedAtMs   int64  `redis:"claimed_at_ms"`
+	CompletedAtMs int64  `redis:"completed_at_ms"`
+	ClaimToken    string `redis:"claim_token"`
+	LastError     string `redis:"last_error"`
+}
+
+func NewTask(name string, payload []byte) *Task {
+	t := &Task{
+		Id:      uuid.New().String(),
+		Name:    name,
+		Payload: string(payload),
+	}
+
+	return t
+}
